dc5ef98a76aad04a8d7cfa526081c83176150d1f
softwaretestlab <35885117+softwaretestlab@users.noreply.github.com>
2024-12-24 10:43:55 +1100

    Update AT01_LearnGit02.java

--- src/test/java/org/example/AT01_LearnGit02.java
+++ src/test/java/org/example/AT01_LearnGit02.java
@@ -9,5 +9,6 @@ public class AT01_LearnGit02 {
         System.out.println("this is test02");
         System.out.println("this is test03");
          System.out.println("this is test04");
+        System.out.println("this is test06");
     }
 }
